044161fc0eb6a7d7a90209ffc0c24f45a3d3d9a5
Leonardo Milano <milanoleonardo@yahoo.it>
2020-03-10 13:54:46 +0100

    Minor cosmetics to the risk plot: show detection window

--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 import utils
+import matplotlib.patches as mpatches
+from utils import DETECTION_THRESH
 
 PLOT_DIR = "plots"
 
@@ -9,16 +11,21 @@ def plot_risk(risk, threshold, real_outbreaks, admin2_pcode):
     # TODO:add date and not only the #month
     detections = utils.get_detections(risk, threshold)
     fig, ax = plt.subplots()
-    ax.plot(risk)
-    ax.axhline(threshold, c='k')
+    ax.plot(risk,lw=3)
+    ax.axhline(threshold, c='gray',ls='--')
     for i, d in enumerate(detections):
         ax.axvline(x=d, lw=2, alpha=0.5, c='y', label="detections" if i == 0 else None)
+        ax.fill_between(range(d,d+DETECTION_THRESH),risk[d:d+DETECTION_THRESH],alpha=0.5, color='y')
     for i, r in enumerate(real_outbreaks):
         ax.axvline(x=r, c='r', label="outbreaks" if i == 0 else None)
     ax.set_ylabel('risk')
     ax.set_xlabel('month')
     ax.set_ylim(0, 1)
-    ax.legend()
+    handles, _ = ax.get_legend_handles_labels()
+    patch = mpatches.Patch(color='y', alpha=0.5, label='Detection window')
+    handles.append(patch) 
+    plt.legend(handles=handles, loc='upper center')
+    # ax.legend()
     fig.savefig(f'{PLOT_DIR}/risk_{admin2_pcode}.png')
     plt.close(fig)
 
